08b9e6cfe4d20c6e8e16bed064c42987654c3fe9
DeltaCodon <117631438+DeltaCodon@users.noreply.github.com>
2024-08-22 22:56:05 -0500

    Implemented more fields for sign up page // EOD commit

--- modern_banking_app/components/AuthForm.tsx
+++ modern_banking_app/components/AuthForm.tsx
@@ -8,25 +8,22 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+import { Form } from "@/components/ui/form";
 import CustomInputs from "./CustomInputs";
 import { authFormSchema } from "@/lib/utils";
+import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 const AuthForm = ({ type }: { type: string }) => {
+  const router = useRouter();
   const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const formSchema = authFormSchema(type);
 
   // 1. Define your form.
-  const form = useForm<z.infer<typeof authFormSchema>>({
-    resolver: zodResolver(authFormSchema),
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
     defaultValues: {
       email: "",
       password: "",
@@ -34,11 +31,31 @@ const AuthForm = ({ type }: { type: string }) => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof authFormSchema>) {
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    console.log(values);
-  }
+    setIsLoading(true);
+
+    try {
+      // Sign up with Appwite & create plain link token
+
+      if (type === "sign-up") {
+        // const newUser = await signUp(data);
+        // setUser(newUser);
+      }
+      if (type === "sign-in") {
+        // const response = await SignIn({
+        //   email: data.email,
+        //   password: data.password,
+        // });
+        // if (response) router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <section className="auth-form">
@@ -72,6 +89,72 @@ const AuthForm = ({ type }: { type: string }) => {
         <>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              {type === "sign-up" && (
+                <>
+                  <div className="flex gap-4">
+                    <CustomInputs // First Name field
+                      control={form.control}
+                      name="firstName"
+                      label="First Name"
+                      placeholder="Enter your first name"
+                      type="text"
+                    />
+                    <CustomInputs // Last Name field
+                      control={form.control}
+                      name="lastName"
+                      label="Last Name"
+                      placeholder="Enter your last name"
+                      type="text"
+                    />
+                  </div>
+                  <CustomInputs // Address field
+                    control={form.control}
+                    name="address1"
+                    label="Address"
+                    placeholder="Enter your current residental address"
+                    type="text"
+                  />
+                  <CustomInputs // City field
+                    control={form.control}
+                    name="city"
+                    label="City"
+                    placeholder="Enter your current city"
+                    type="text"
+                  />
+                  <div className="flex gap-4">
+                    <CustomInputs // State Field
+                      control={form.control}
+                      name="state"
+                      label="State"
+                      placeholder="ex: Tx"
+                      type="text"
+                    />
+                    <CustomInputs // Postal Code Field
+                      control={form.control}
+                      name="postalCode"
+                      label="postalCode"
+                      placeholder="ex: 78043"
+                      type="text"
+                    />
+                  </div>
+                  <div className="flex gap-4">
+                    <CustomInputs // Date Of Birth Field
+                      control={form.control}
+                      name="dateOfBirth"
+                      label="Date of Birth"
+                      placeholder="mm/dd/yyyy"
+                      type="date"
+                    />
+                    <CustomInputs // Social Security Number Field
+                      control={form.control}
+                      name="ssn"
+                      label="Social Securtiy Number"
+                      placeholder="ex: ***-**-****"
+                      type="text"
+                    />
+                  </div>
+                </>
+              )}
               <CustomInputs
                 control={form.control}
                 name="email"
@@ -86,9 +169,37 @@ const AuthForm = ({ type }: { type: string }) => {
                 placeholder="Enter your password"
                 type="password"
               />
-              <Button type="submit">Submit</Button>
+
+              <div className="flex flex-col gap-4">
+                <Button type="submit" disabled={isLoading} className="form-btn">
+                  {isLoading ? (
+                    <>
+                      <Loader2 size={20} className="animate-spin" /> &nbsp;
+                      Loading...
+                    </>
+                  ) : type === "sign-in" ? (
+                    "Sign In"
+                  ) : (
+                    "Sign Up"
+                  )}
+                </Button>
+              </div>
             </form>
           </Form>
+
+          <footer className="flex justify-center gap-1">
+            <p className="text-14 font-nromal tex-gray-600">
+              {type === "sign-in"
+                ? "Don't have an account?"
+                : "Already have an account?"}
+            </p>
+            <Link
+              href={type === "sign-in" ? "/sign-up" : "/sign-in"}
+              className="form-link"
+            >
+              {type === "sign-in" ? "Sign Up" : "Sign In"}
+            </Link>
+          </footer>
         </>
       )}
     </section>
--- modern_banking_app/lib/utils.ts
+++ modern_banking_app/lib/utils.ts
@@ -195,8 +195,21 @@ export const getTransactionStatus = (date: Date) => {
   return date > twoDaysAgo ? "Processing" : "Success";
 };
 
-export const authFormSchema = z.object({
-  email: z.string().email(),
-  username: z.string().min(8),
-  password: z.string().min(8),
-});
+export const authFormSchema = (type: string) =>
+  z.object({
+    // sign up page
+    firstName: type === "sign-in" ? z.string().optional() : z.string().min(3),
+    lastName: type === "sign-in" ? z.string().optional() : z.string().min(3),
+    address1: type === "sign-in" ? z.string().optional() : z.string().max(50),
+    city: type === "sign-in" ? z.string().optional() : z.string().max(15),
+    state:
+      type === "sign-in" ? z.string().optional() : z.string().min(2).max(2),
+    postalCode:
+      type === "sign-in" ? z.string().optional() : z.string().min(3).max(6),
+    dateOfBirth: type === "sign-in" ? z.string().optional() : z.string().min(8),
+    ssn: type === "sign-in" ? z.string().optional() : z.string().min(9).max(9),
+    // sign in and sign up page
+    email: z.string().email(),
+    username: z.string().min(8),
+    password: z.string().min(8).max(30),
+  });
