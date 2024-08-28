6a4130d5eed0d10a18dbac05dfbcf1ef7416ffbd
DeltaCodon <117631438+DeltaCodon@users.noreply.github.com>
2024-08-28 13:13:59 -0500

    implemtented sentry //midday commit

--- modern_banking_app/components/AuthForm.tsx
+++ modern_banking_app/components/AuthForm.tsx
@@ -13,7 +13,7 @@ import CustomInputs from "./CustomInputs";
 import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { getLoggedInUser, signIn, signUp } from "@/lib/actions/user.actions";
+import { signIn, signUp } from "@/lib/actions/user.actions";
 // import PlaidLink from "./PlaidLink";
 
 const AuthForm = ({ type }: { type: string }) => {
